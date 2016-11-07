84b22fbd53fc61eabcff7ed6c796a0090211417f
Michael S Siddiqui <siddiqui@alumni.upenn.edu>
2016-11-06 20:14:24 -0800

    add python script for executing check_the_weather from the command line

---
+++ weather_script.py
@@ -0,0 +1,18 @@
+# for command line execution
+# of check_the_weather
+
+from weather.weather import check_the_weather
+import sys
+
+def main(location):
+    output = check_the_weather(location)
+    return output
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        location = sys.argv[1]
+    else:
+        location = ''
+    weather_info = main(location)
+    print weather_info
+
