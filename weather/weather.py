ee835e96cff5a80a679e930c98cb33cac63a119a
Michael S Siddiqui <siddiqui@alumni.upenn.edu>
2016-11-06 19:56:53 -0800

    add correct text for check_the_weather function definition in weather module

--- weather/weather.py
+++ weather/weather.py
@@ -29,14 +29,25 @@ def interactive_location():
 
 # function to check the weather via
 # an API call to wunderground
-def interactive_location():
-    '''helper function to ask the user for what location
-    would he/sheS like to check the weather conditions
-    '''
-    print 'choose where to check the weather'
-    location = raw_input('location: ')
+def check_the_weather(location=''):
+    """check the current weather and temp
+    for a specified location
+    """
+    if not location:
+        location = interactive_location()
+    url = url_root + API_KEY + geo_conditions + location
+    response = requests.get(url + '.json')
+    weather_dict = json.loads(response.content)
+    try:
+        city = weather_dict['location']['city']
+        state = weather_dict['location']['state']
+        temp = weather_dict['current_observation']['temperature_string']
+        weather = weather_dict['current_observation']['weather']
+    except:
+        error = "something didn't work"
+        return error
+    line1 = "Weather for " + city + ', ' + state + ' is'
+    line2 = weather + ' and the temperature is ' + temp
+    output_string = line1 + '\n' + line2
+    return output_string
 
-    # wunderground can choose location by ip address
-    if location in ['here', '']:
-        location = 'autoip'
-    return location
